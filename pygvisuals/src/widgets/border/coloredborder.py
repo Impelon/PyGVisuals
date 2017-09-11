e8b964234d5fec252c3ea35c5d31002f27e4ed27
Ovi T. (Impelon) <ovi.tatar@googlemail.com>
2017-09-11 18:05:31 +0200

    Upload border-files

---
+++ pygvisuals/src/widgets/border/coloredborder.py
@@ -0,0 +1,39 @@
+# -*- coding: cp1252 -*-
+
+import pygame
+import border
+
+class ColoredBorder(border.Border):
+
+    """
+    Border with a color
+    """
+
+    def __init__(self, width, height, color):
+        """
+        Initialisation of a ColoredBorder
+
+        parameters:     int width of the ColoredBorder on the left and right sides or tuple for each side specifically
+                        int height of the ColoredBorder on the top and bottom sides or tuple for each side specifically
+                        tuple of format pygame.Color representing the ColoredBorder's color
+        return values:  -
+        """
+        super(ColoredBorder, self).__init__(width, height)
+        self.color = color
+
+    def getBorderedImage(self, surface):
+        """
+        Draw the ColoredBorder and return the bordered result
+
+        parameters:     pygame.Surface the image to be bordered
+        return values:  pygame.Surface the bordered result
+        """
+        if isinstance(surface, pygame.Surface) and not self.isEmptyBorder():
+            rect        = surface.get_rect()
+            size        = self.getBounds(rect)
+            bordered    = pygame.Surface(size.size, 0, surface)
+            bordered.fill(self.color)
+            bordered.fill((0, 0, 0, 0), rect.move(self.left, self.top))
+            bordered.blit(surface, (self.left, self.top))
+            return bordered
+        return surface
