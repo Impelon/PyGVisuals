46d3845fef2a23ecbecde6c9f73c0f5acda6b98d
Ovi T. (Impelon) <ovi.tatar@googlemail.com>
2017-09-12 09:47:19 +0200

    Upload PositionMap & Visual Test
    - Added the PositionMap and its visual testscript

---
+++ pygvisuals/src/__experimental/positionmap.py
@@ -0,0 +1,206 @@
+# -*- coding: cp1252 -*-
+
+import pygame
+
+def createByImage(path = "map.png"):
+    """
+    Create a PositionMap by using a imagefile
+    pixels with RGB-values of 0 (black) will be interpreted as invalid positions aka. obstacles
+
+    parameter:      string path leading to the imagefile
+    return values:  PositionMap the result
+    """
+    try:
+        mapdata = pygame.image.load(path)
+    except:
+        m = PositionMap()
+        m.setWidth(1)
+        m.setHeight(1)
+        return m
+    return createBySurface(mapdata)
+
+def createBySurface(surface):
+    """
+    Create a PositionMap by using a Surface
+    pixels with RGB-values of 0 (black) will be interpreted as invalid positions aka. obstacles
+
+    parameter:      pygame.Surface the surface to interpret
+    return values:  PositionMap the result
+    """
+    m       = PositionMap()
+    invalid = set()
+    size    = surface.get_size()
+    black   = surface.map_rgb((0, 0, 0))
+    surface = pygame.PixelArray(surface)
+    m.setWidth(size[0])
+    m.setHeight(size[1])
+    for x in xrange(size[0]):
+        for y in xrange(size[1]):
+            try:
+                if surface[x][y] == black:
+                    invalid.add((x, y))
+            except:
+                pass
+    m._invalidPositions = frozenset(invalid)
+    return m
+
+def createByList(l = []):
+    """
+    Create a PositionMap by using a list of strings
+    the length of each list element (string) and the length of the list correspond to the width and height of the PositionMap respectively
+    all characters except spaces will be interpreted as invalid positions aka. obstacles
+    
+    parameter:      list list of strings following the described format
+    return values:  PositionMap the result
+    """
+    m       = PositionMap()
+    invalid = set()
+    w       = 0
+    if not l:
+        m.setWidth(1)
+        m.setHeight(1)
+        return m
+    for y in xrange(len(l)):
+        ln = str(l[y])
+        for x in xrange(len(ln)):
+            if ln[x] != " ":
+                invalid.add((x, y))
+        if len(ln) > w:
+            w = len(ln)
+    m.setWidth(w)
+    m.setHeight(len(l))
+    m._invalidPositions = frozenset(invalid)
+    return m
+
+def scale(m, scale = 1):
+    """
+    Scale a PositionMap with a scalar (out-of-place)
+    
+    parameter:      PositionMap the PositionMap, which should be scaled
+                    int/tuple the scalar or a tuple containing a scalar for each dimension
+    return values:  PositionMap the result
+    """
+    if isinstance(scale, tuple):
+        scaleX = abs(scale[0])
+        scaleY = abs(scale[1])
+    else:
+        scaleX = abs(scale)
+        scaleY = abs(scale)
+    resolution = (int(m.getWidth() * scaleX), int(m.getHeight() * scaleY))
+    return createBySurface(pygame.transform.scale(m.toSurface(), resolution))
+
+
+class PositionMap:
+
+    """
+    Class for a map of valid and invalid positions aka. obstacles
+    """
+
+    def __init__(self):
+        """
+        Initialisation of a PositionMap
+
+        parameter:      -
+        return values:  -
+        """
+        self._width     = 0
+        self._height    = 0
+        self._invalidPositions = frozenset()
+    
+    def getWidth(self):
+        """
+        Return the width of the PositionMap
+
+        parameter:      -
+        return values:  int width of the PositionMap
+        """
+        return self._width
+
+    def getHeight(self):
+        """
+        Return the height of the PositionMap
+
+        parameter:      -
+        return values:  int height of the PositionMap
+        """
+        return self._height
+
+    def getLengthInvalidPositions(self):
+        """
+        Return the amount of invalid positions of the PositionMap
+
+        parameter:      -
+        return values:  int amount of invalid positions of the PositionMap
+        """
+        return len(self._invalidPositions)
+
+    def setWidth(self, width):
+        """
+        Set the width of the PositionMap, in case it has not been set yet
+
+        parameter:      int width of the PositionMap
+        return values:  -
+        """
+        if not self._width:
+            self._width = int(width)
+
+    def setHeight(self, height):
+        """
+        Set the height of the PositionMap, in case it has not been set yet
+
+        parameter:      int height of the PositionMap
+        return values:  -
+        """
+        if not self._height:
+            self._height = int(height)
+
+    def isPositionValid(self, x, y):
+        """
+        Return if a position is valid according to the PositionMap, aka. if there are any obstacles
+
+        parameter:      int x-coordinate of the position to check
+                        int y-coordinate of the position to check
+        return values:  bool whether the position is valid according to the PositionMap
+        """
+        if x >= self._width:
+            return False
+        if y >= self._height:
+            return False
+        if x < 0:
+            return False
+        if y < 0:
+            return False
+        return not (x, y) in self._invalidPositions
+    
+    def isRectValid(self, rect):
+        """
+        Return if an area/rect is completely valid according to the PositionMap, aka. if there are any obstacles inside the rect
+
+        parameter:      pygame.Rect the area/rect to check
+        return values:  bool whether the area/rect is completely valid according to the PositionMap
+        """
+        for x in xrange(rect.width):
+            for y in xrange(rect.height):
+                if not self.isPositionValid(x + rect.x, y + rect.y):
+                    return False
+        return True
+
+    def toSurface(self):
+        """
+        Return a Surface representing the PositionMap
+        invalid positions will be represented by pixels with RGB-values of 0 (black)
+        valid positions ill be represented by pixels with RGB-values of 255 (white)
+
+        parameter:      -
+        return values:  pygame.Surface the result
+        """
+        surface = pygame.Surface((self.getWidth(), self.getHeight()), 0, 8)
+        surface.fill((255, 255, 255))
+        black   = surface.map_rgb((0, 0, 0))
+        mapdata = pygame.PixelArray(surface)
+        for pos in self._invalidPositions:
+            try:
+                mapdata[pos[0]][pos[1]] = black
+            except:
+                pass
+        return surface
---
+++ pygvisuals/src/__experimental/test_positionmap.py
@@ -0,0 +1,267 @@
+# -*- coding: cp1252 -*-
+
+import positionmap
+import pygame, pygame.gfxdraw
+import sys, random, time
+from pygame.locals import *
+
+pygame.init()
+
+m           = positionmap.PositionMap()
+screen      = pygame.display.set_mode((1000, 800), 0, 32)
+rc          = False
+stop        = False
+mode        = "n"
+testhelp    = ("Press following keys for respective actions:",
+               "t: Toggle color randomness (pixel-drawingmode only)",
+               "i: Print info about the status of the PositionMap",
+               "b: Create the PositionMap from an image in the same folder (map.png)",
+               "l: Create the PositionMap from a list hardcoded in the sourcecode",
+               "+: Scale up the PositionMap by a factor of 2",
+               "-: Scale down the PositionMap by a factor of 0.5",
+               "F5 or F11: Scale up the PositionMap so it fits the entire screen",
+               "Down-key: Create an imagefile (result.png) with the current state of the PositionMap",
+               "n: Switch to normal-drawing mode; Draws the PositionMap according to mapobject.toSurface()",
+               "p: Switch to pixel-drawing mode; Draws the PositionMap by drawing a row of pixels individually",
+               "s: Switch to slow pixel-drawing mode; Draws the PositionMap drawing each pixel individually",
+               "r: Switch to slow pixel-drawing mode; Draws the PositionMap drawing each pixel individually at a random position")
+
+def createFromImage():
+    """
+    Create the PositionMap by using an imagefile (map.png)
+
+    parameter:      -
+    return values:  -
+    """
+    global m
+    m = positionmap.createByImage()
+
+def createFromList():
+    """
+    Create the PositionMap by using a list
+    
+    parameter:      -
+    return values:  -
+    """
+    global m
+    l = (
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    "W                                W",
+    "W                                W",
+    "W   WWW        WW       WWWWW    W",
+    "W   W         WWWWW              W",
+    "W   W   WWW       W              W",
+    "W   W     W       W              W",
+    "W   W     W  W       WWW        WW",
+    "W   WWW   W                W W   W",
+    "W     W   W   WWW                W",
+    "WW   WW               WWWWW W    W",
+    "W   W                            W",
+    "W   W   WWWW           WWW       W",
+    "W                                W",
+    "W   WW    W                      W",
+    "W         WWWWWW                 W",
+    "W         W                      W",
+    "W         W          WWWWW       W",
+    "W                        W       W",
+    "WWWW                      W      W",
+    "W                 WWWWW          W",
+    "W                  W             W",
+    "W                 W              W",
+    "W   WWWWW                        W",
+    "W                                W",
+    "W             WWWWWWWW           W",
+    "W                  W             W",
+    "WWWWWWW                   WWWWWWWW",
+    "W                                W",
+    "W                                W",
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    )
+    m = positionmap.createByList(l)
+
+def saveMap():
+    """
+    Save the PositionMap as an imagefile
+    
+    parameter:      -
+    return values:  -
+    """
+    global m
+    try:
+        pygame.image.save(m.toSurface(), "result.png")
+    except:
+        pass
+
+def mapScaleUp():
+    """
+    Expand the PositionMap
+    
+    parameter:      -
+    return values:  -
+    """
+    global m
+    m = positionmap.scale(m, 2)
+
+def mapScaleDown():
+    """
+    Reduce the size of the PositionMap
+    
+    parameter:      -
+    return values:  -
+    """
+    global m
+    m = positionmap.scale(m, 0.5)
+
+def mapFullscreen():
+    """
+    Expand the PositionMap to window-size
+    
+    parameter:      -
+    return values:  -
+    """
+    global m, screen
+    monitorinfo = pygame.display.Info()
+    m = positionmap.scale(m, (screen.get_width() / max(m.getWidth(), 1), screen.get_height() / max(m.getHeight(), 1)))
+
+def mapDraw():
+    """
+    Draw the PositionMap by drawing the objects image representation (map.toSurface())
+    
+    parameter:      -
+    return values:  -
+    """
+    global m, screen
+    stop = False
+    color = (250, 150, 100)
+    screen.blit(m.toSurface(), (0, 0))
+    handleInput()
+    pygame.display.update()
+
+def mapDrawPixel():
+    """
+    Draw the PositionMap by drawing a row of pixels
+    
+    parameter:      -
+    return values:  -
+    """
+    global m, screen, rc, stop
+    stop = False
+    color = (250, 150, 100)
+    for x in xrange(m.getWidth()):
+        for y in xrange(m.getHeight()):
+            if rc:
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            if not m.isPositionValid(x, y):
+                pygame.gfxdraw.pixel(screen, x, y, color)
+        handleInput()
+        pygame.display.update()
+        if stop:
+            return
+
+def mapDrawPixelSlow():
+    """
+    Draw the PositionMap by drawing each pixels individually
+    
+    parameter:      -
+    return values:  -
+    """
+    global m, screen, rc, stop
+    stop = False
+    color = (250, 150, 100)
+    for x in xrange(m.getWidth()):
+        for y in xrange(m.getHeight()):
+            if rc:
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            if not m.isPositionValid(x, y):
+                pygame.gfxdraw.pixel(screen, x, y, color)
+            handleInput()
+            pygame.display.update()
+            if stop:
+                return
+
+def mapDrawPixelRandom():
+    """
+    Draw the PositionMap by drawing a random pixel
+    
+    parameter:      -
+    return values:  -
+    """
+    global m, screen, rc, stop
+    stop = False
+    color = (250, 150, 100)
+    while True:
+        for n in range(m.getHeight()):
+            x, y = random.randint(0, m.getWidth()), random.randint(0, m.getHeight())
+            if rc:
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            if not m.isPositionValid(x, y):
+                pygame.gfxdraw.pixel(screen, x, y, color)
+        handleInput()
+        pygame.display.update()
+        if stop:
+            screen.fill((255, 255, 255))
+            return
+
+def printHelp():
+    """
+    Print the help-information
+    
+    parameter:      -
+    return values:  -
+    """
+    for ln in testhelp:
+        print ln
+
+def handleInput():
+    """
+    Handle User-Input
+    
+    parameter:      -
+    return values:  -
+    """
+    global rc, m, stop, mode
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            key = event.unicode.encode("ascii", "ignore").lower()
+
+            if key == "t":
+                rc = not rc
+            if key == "i":
+                print "invalid Positions:", m.getLengthInvalidPositions(), "possible Positions:", m.getWidth() * m.getHeight()
+            if key == "h":
+                printHelp()
+            if key == "b":
+                createFromImage()
+            if key == "l":
+                createFromList()
+            if key == "+":
+                mapScaleUp()
+            if key == "-":
+                mapScaleDown()
+            if event.key in (pygame.K_F5, pygame.K_F11):
+                mapFullscreen()
+            if event.key == pygame.K_DOWN:
+                saveMap()
+            
+            if key in ("n", "p", "s", "r"):
+                mode = key
+                stop = True
+
+if __name__ == "__main__":
+    createFromImage()
+    printHelp()
+    while True:
+        if mode == "n":
+            mapDraw()
+        elif mode == "p":
+            mapDrawPixel()
+        elif mode == "s":
+            mapDrawPixelSlow()
+        elif mode == "r":
+            mapDrawPixelRandom()
+        else:
+            mapDraw()
+        screen.fill((255, 255, 255))
