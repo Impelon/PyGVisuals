49be7e90262b90d88ea225bfc5e2b5360881b51b
Impelon <ovi.tatar@googlemail.com>
2020-04-09 14:09:29 +0200

    Use consistent double-quote strings in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from setuptools import setup
 
-setup(name='PyGVisuals',
-      version='0.7',
+setup(name="PyGVisuals",
+      version="0.7",
       description="A collection of Classes to create GUI's and more based purely on pygame",
-      url='https://github.com/Impelon/PyGVisuals',
-      author='Impelon & kjkriegel',
-      author_email='pygvisuals@kjkriegel.de',
-      license='BSD-2-Clause',
-      packages=['pygvisuals'],
+      url="https://github.com/Impelon/PyGVisuals",
+      author="Impelon & kjkriegel",
+      author_email="pygvisuals@kjkriegel.de",
+      license="BSD-2-Clause",
+      packages=["pygvisuals"],
       install_requires=["pygame"],
       zip_safe=False,)
