92e378a4fa27661846eb50fe22e2ee75011c8b33
Impelon <ovi.tatar@googlemail.com>
2020-04-13 10:18:21 +0200

    Add more direct control to how the RoundedBorder is drawn

--- pygvisuals/borders/rounded_border.py
+++ pygvisuals/borders/rounded_border.py
@@ -13,7 +13,7 @@ class RoundedBorder(ColoredBorder):
     Border with rounded corners, which can be colored.
     """
 
-    def __init__(self, width, height, color, radius):
+    def __init__(self, width, height, color, radius, surface_above_border = False):
         """
         Initialisation of a RoundedBorder.
         The rounded rectangle is produced by placing 4 circles on the side of the rectangle as described here:
@@ -29,15 +29,18 @@ class RoundedBorder(ColoredBorder):
             color: A color-like object that can be interpreted as a color by pygame (such as a tuple with RGB values).
                 This will be used as the color for the border.
             radius: An integer denoting the radius in pixels of the circles on the corners of the RoundedBorder.
+            surface_above_border: A boolean controling whether the border will be drawn behind the surface
+                or on top of it. The default value is False, meaning the border will be drawn on top of the surface.
         """
         super(RoundedBorder, self).__init__(width, height, color)
         self.radius = radius
-        self.remove_background_after_draw = False
-        self.draw_surface_above_border = False
+        self.remove_background_after_draw = surface_above_border
+        self.draw_surface_above_border = surface_above_border
 
     def _drawBorder(self, surface, original_rect, bordered_rect):
         surface.blit(self._getRoundRect(bordered_rect, self.color), (0, 0))
-        surface.blit(self._getRoundRect(original_rect, (255, 255, 255, 255)), (self.left, self.top), special_flags=pygame.BLEND_RGBA_SUB)
+        if not self.remove_background_after_draw:
+            surface.blit(self._getRoundRect(original_rect, (255, 255, 255, 255)), (self.left, self.top), special_flags=pygame.BLEND_RGBA_SUB)
         return surface
 
     def _getRoundRect(self, rect, color):
