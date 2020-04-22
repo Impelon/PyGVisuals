74ef8339fd6b5ef39a3617b1bd92ef37b9d6d93c
Impelon <ovi.tatar@googlemail.com>
2020-04-22 11:36:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ from setuptools import setup
 
 setup(name="PyGVisuals",
       version="0.7",
-      description="A collection of Classes to create GUI's and more based purely on pygame",
+      description="A collection of classes to create GUIs and more based purely on pygame.",
       url="https://github.com/Impelon/PyGVisuals",
       author="Impelon & kjkriegel",
       author_email="pygvisuals@kjkriegel.de",
       license="BSD-2-Clause",
-      packages=["pygvisuals"],
+      packages=["pygvisuals",
+                "pygvisuals.borders",
+                "pygvisuals.designs",
+                "pygvisuals.io",
+                "pygvisuals.util",
+                "pygvisuals.widgets",],
       install_requires=["pygame"],
       zip_safe=False,)
